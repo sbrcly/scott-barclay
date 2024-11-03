1aec096a6424456284292a3601e99c03628e9ed9
sbrcly <sbrcly@gmail.com>
2024-11-02 17:10:09 -0700

    test commit

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -5,7 +5,9 @@ export default function Root() {
     return (
         <>
             <MainNavigation />
-            <Outlet />
+            <main>
+                <Outlet />
+            </main>
         </>
     )
 };
