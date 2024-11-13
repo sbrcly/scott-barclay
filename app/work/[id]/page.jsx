77be21212b0eb0cfd4ea0c89fb0d77fd651d90da
sbrcly <sbrcly@gmail.com>
2024-11-13 14:41:29 -0800

    add dynamic projects routing

---
+++ app/work/[id]/page.jsx
@@ -0,0 +1,19 @@
+export default async function ProjectDetails({ params }) {
+    const { id } = await params
+
+    return (
+        <main
+            style={{
+                position: 'absolute',
+                top: '15%',
+                left: '50%',
+                transform: 'translateX(-50%)',
+                textAlign: 'center',
+                zIndex: '-1'
+            }}
+        >
+            <h1>Project Details Page</h1>
+            <h3>{id}</h3>
+        </main>
+    )
+};
